97c9a30630ecbbd430d12b9b76cee1c090cb5aa5
danilotpnta-elsevier <d.toapantabrahona@elsevier.com>
2025-01-21 15:44:45 +0000

    [wip]: debugging to avoid CUDA out of memory errors

--- eval/eval_article_quality.py
+++ eval/eval_article_quality.py
@@ -20,38 +20,43 @@ from evaluation_prometheus import get_grading_dict, preprocess_text
 from evaluation_trim_length import process_document
 from metrics import article_entity_recall, compute_rouge_scores
 
-logging.basicConfig(level=logging.INFO, format='%(asctime)s - %(levelname)s - %(message)s')
+from src.utils import get_device
+from config.constants import HF_CACHE_DIR
+
+logging.basicConfig(
+    level=logging.INFO, format="%(asctime)s - %(levelname)s - %(message)s"
+)
 
 
 class ColoredFormatter(logging.Formatter):
     COLORS = {
-        'WARNING': '\033[93m',  # Yellow
-        'INFO': '\033[97m',  # White
-        'DEBUG': '\033[92m',  # Green
-        'CRITICAL': '\033[94m',  # Blue
-        'ERROR': '\033[91m',  # Red
-        'RESET': '\033[0m',  # Reset
+        "WARNING": "\033[93m",  # Yellow
+        "INFO": "\033[97m",  # White
+        "DEBUG": "\033[92m",  # Green
+        "CRITICAL": "\033[94m",  # Blue
+        "ERROR": "\033[91m",  # Red
+        "RESET": "\033[0m",  # Reset
     }
 
     def format(self, record):
-        color = self.COLORS.get(record.levelname, self.COLORS['RESET'])
-        record.levelname = color + record.levelname + self.COLORS['RESET']
-        record.msg = color + str(record.msg) + self.COLORS['RESET']
+        color = self.COLORS.get(record.levelname, self.COLORS["RESET"])
+        record.levelname = color + record.levelname + self.COLORS["RESET"]
+        record.msg = color + str(record.msg) + self.COLORS["RESET"]
         return super().format(record)
 
 
 def load_str(path):
-    with open(path, 'r') as f:
-        return '\n'.join(f.readlines())
+    with open(path, "r") as f:
+        return "\n".join(f.readlines())
 
 
 def load_json(path):
-    with open(path, 'r') as f:
+    with open(path, "r") as f:
         return json.load(f)
 
 
 def dump_json(data, path):
-    with open(path, 'w') as f:
+    with open(path, "w") as f:
         json.dump(data, f, indent=2)
 
 
@@ -64,84 +69,124 @@ def assert_int(x):
 
 
 def main(args):
+
+
+
     logger.info(f"loading tokenizer {args.tokenizer} and model {args.model}")
     tokenizer = AutoTokenizer.from_pretrained(args.tokenizer)
-    model = LlamaForCausalLM.from_pretrained(args.model, device_map="auto")
+    model = LlamaForCausalLM.from_pretrained(
+        args.model,
+        device_map="auto",
+        offload_folder=args.offload_dir,
+        max_memory={
+            0: "16GiB",
+            "cpu": "128GiB",
+        },
+    )
 
     df = pd.read_csv(args.input_path)
 
     aggregated_results = {}
 
     for i, row in tqdm(df.iterrows()):
-        import pdb
-        pdb.set_trace()
-        topic = row['topic']
+        # import pdb
+
+        # pdb.set_trace()
+        topic = row["topic"]
         topic_name = topic.replace(" ", "_").replace("/", "_")
         llm_output_path = os.path.join(args.pred_dir, topic_name, args.pred_file_name)
-        assert os.path.exists(llm_output_path), f"llm output path not exists {llm_output_path}"
+        assert os.path.exists(
+            llm_output_path
+        ), f"llm output path not exists {llm_output_path}"
 
-        golden_answer_json = load_json(os.path.join(args.gt_dir, 'json', topic_name + '.json'))
-        golden_answer = load_str(os.path.join(args.gt_dir, 'txt', topic_name + '.txt'))
+        golden_answer_json = load_json(
+            os.path.join(args.gt_dir, "json", topic_name + ".json")
+        )
+        golden_answer = load_str(os.path.join(args.gt_dir, "txt", topic_name + ".txt"))
         golden_answer = preprocess_text(golden_answer)
         llm_output = load_str(llm_output_path)
         llm_output = preprocess_text(llm_output)
         output_file_path = os.path.join(args.result_output_dir, f"{topic_name}.json")
 
         # Prometheus model has a limited context window.
-        trimmed_output_for_rubric_grading = process_document(llm_output_path, max_words=2000)
+        trimmed_output_for_rubric_grading = process_document(
+            llm_output_path, max_words=2000
+        )
 
         evaluation_main_dict = {"topic": topic, "grading": {}}
 
         # Get rubric grading.
         logger.info(f"Processing rubric grading.")
-        grading_dict = get_grading_dict(responses=[trimmed_output_for_rubric_grading],
-                                        topic=topic,
-                                        tokenizer=tokenizer,
-                                        model=model,
-                                        prompt_template_path=args.prompt_template_path,
-                                        rubric_path=args.rubric_path,
-                                        logger=logger)
+        grading_dict = get_grading_dict(
+            responses=[trimmed_output_for_rubric_grading],
+            topic=topic,
+            tokenizer=tokenizer,
+            model=model,
+            prompt_template_path=args.prompt_template_path,
+            rubric_path=args.rubric_path,
+            logger=logger,
+        )
 
         for criteria_description, response_grading_dict in grading_dict.items():
             for response_idx, feedback_dict in response_grading_dict.items():
-                if 'rubric_grading' not in evaluation_main_dict["grading"]:
-                    evaluation_main_dict["grading"] = {"rubric_grading": {criteria_description: feedback_dict}}
+                if "rubric_grading" not in evaluation_main_dict["grading"]:
+                    evaluation_main_dict["grading"] = {
+                        "rubric_grading": {criteria_description: feedback_dict}
+                    }
                 else:
-                    evaluation_main_dict["grading"]["rubric_grading"][criteria_description] = feedback_dict
+                    evaluation_main_dict["grading"]["rubric_grading"][
+                        criteria_description
+                    ] = feedback_dict
 
         # get automatic evaluation score
         logger.info(f"Processing automatic evaluation.")
-        automatic_evaluation_score = compute_rouge_scores(predicted_answer=llm_output, golden_answer=golden_answer)
+        automatic_evaluation_score = compute_rouge_scores(
+            predicted_answer=llm_output, golden_answer=golden_answer
+        )
         evaluation_main_dict["grading"]["auto_grading"] = automatic_evaluation_score
 
         # get named entity overlap with golden answer
         logger.info(f"Processing entity overlap with ground truth")
         evaluation_main_dict["grading"]["entity_recall"] = article_entity_recall(
-            golden_entities=golden_answer_json['flair_entities'],
-            predicted_article=llm_output
+            golden_entities=golden_answer_json["flair_entities"],
+            predicted_article=llm_output,
         )
 
         dump_json(evaluation_main_dict, output_file_path)
 
         if len(aggregated_results) == 0:
-            for k in evaluation_main_dict['grading']['rubric_grading']:
-                aggregated_results[k] = [evaluation_main_dict['grading']['rubric_grading'][k]]
-            for k in evaluation_main_dict['grading']['auto_grading']:
-                aggregated_results[k] = [evaluation_main_dict['grading']['auto_grading'][k]]
-            aggregated_results['entity_recall'] = [evaluation_main_dict['grading']['entity_recall']]
+            for k in evaluation_main_dict["grading"]["rubric_grading"]:
+                aggregated_results[k] = [
+                    evaluation_main_dict["grading"]["rubric_grading"][k]
+                ]
+            for k in evaluation_main_dict["grading"]["auto_grading"]:
+                aggregated_results[k] = [
+                    evaluation_main_dict["grading"]["auto_grading"][k]
+                ]
+            aggregated_results["entity_recall"] = [
+                evaluation_main_dict["grading"]["entity_recall"]
+            ]
         else:
-            for k in evaluation_main_dict['grading']['rubric_grading']:
-                aggregated_results[k].append(evaluation_main_dict['grading']['rubric_grading'][k])
-            for k in evaluation_main_dict['grading']['auto_grading']:
-                aggregated_results[k].append(evaluation_main_dict['grading']['auto_grading'][k])
-            aggregated_results['entity_recall'].append(evaluation_main_dict['grading']['entity_recall'])
+            for k in evaluation_main_dict["grading"]["rubric_grading"]:
+                aggregated_results[k].append(
+                    evaluation_main_dict["grading"]["rubric_grading"][k]
+                )
+            for k in evaluation_main_dict["grading"]["auto_grading"]:
+                aggregated_results[k].append(
+                    evaluation_main_dict["grading"]["auto_grading"][k]
+                )
+            aggregated_results["entity_recall"].append(
+                evaluation_main_dict["grading"]["entity_recall"]
+            )
 
     # compute average score
     logger.info(f"Computing average score.")
     avg_results = {}
     for k in aggregated_results:
         if type(aggregated_results[k][0]) is dict:
-            avg_results[k] = sum([float(x['score']) for x in aggregated_results[k]]) / len(aggregated_results[k])
+            avg_results[k] = sum(
+                [float(x["score"]) for x in aggregated_results[k]]
+            ) / len(aggregated_results[k])
         else:
             avg_results[k] = sum(aggregated_results[k]) / len(aggregated_results[k])
         print(f"{k}: {avg_results[k]}")
@@ -154,36 +199,70 @@ if __name__ == "__main__":
     logger = logging.getLogger(__name__)
     logger.setLevel(logging.DEBUG)
     console_handler = logging.StreamHandler()
-    formatter = ColoredFormatter('%(levelname)s: %(message)s')
+    formatter = ColoredFormatter("%(levelname)s: %(message)s")
     console_handler.setFormatter(formatter)
     logger.addHandler(console_handler)
 
     # command line argument
     parser = argparse.ArgumentParser()
-    parser.add_argument('--input-path', type=str,
-                        help='Using csv file to store topic and ground truth url at present.')
-    parser.add_argument('--pred-dir',
-                        help='Directory to the file containing the LLM output.')
-    parser.add_argument('--gt-dir',
-                        help='Directory to the file containing the human-written articles.')
-    parser.add_argument('--result-output-dir',
-                        help='Directory to store the evaluation results. '
-                             'Each article evaluation will be saved as separate file named after {topic_name}.json')
-    parser.add_argument('--pred-file-name', help='Name of the article file.')
-    parser.add_argument("--prompt-template-path", default="./eval_prometheus_no_ref.prompt",
-                        help='path to evaluation prometheus prompt template')
-    parser.add_argument("--rubric-path", default="./eval_rubric_5.json", help='path to rubric json file')
+    parser.add_argument(
+        "--input-path",
+        type=str,
+        help="Using csv file to store topic and ground truth url at present.",
+    )
+    parser.add_argument(
+        "--pred-dir", help="Directory to the file containing the LLM output."
+    )
+    parser.add_argument(
+        "--gt-dir", help="Directory to the file containing the human-written articles."
+    )
+    parser.add_argument(
+        "--result-output-dir",
+        help="Directory to store the evaluation results. "
+        "Each article evaluation will be saved as separate file named after {topic_name}.json",
+    )
+    parser.add_argument(
+        "--pred-file-name",
+        default="storm_gen_article_polished.txt",
+        help="Name of the article file.",
+    )
+    parser.add_argument(
+        "--prompt-template-path",
+        default="./eval_prometheus_no_ref.prompt",
+        help="path to evaluation prometheus prompt template",
+    )
+    parser.add_argument(
+        "--rubric-path", default="./eval_rubric_5.json", help="path to rubric json file"
+    )
 
-    parser.add_argument('--tokenizer', default="meta-llama/Llama-2-7b-chat-hf")
-    parser.add_argument('--model',
-                        choices=["kaist-ai/prometheus-13b-v1.0", "kaist-ai/prometheus-7b-v1.0"],
-                        default="kaist-ai/prometheus-13b-v1.0",
-                        help="Model to use for rubric evaluation.")
+    parser.add_argument("--tokenizer", default="meta-llama/Llama-2-7b-chat-hf")
+    parser.add_argument(
+        "--model",
+        choices=["kaist-ai/prometheus-13b-v1.0", "kaist-ai/prometheus-7b-v1.0"],
+        # default="kaist-ai/prometheus-13b-v1.0",
+        default="kaist-ai/prometheus-7b-v1.0",
+        help="Model to use for rubric evaluation.",
+    )
+    parser.add_argument(
+        "--offload_dir",
+        default=os.path.join(HF_CACHE_DIR, "offload"),
+        help="Directory to offload the model and tokenizer to.",
+    )
     args = parser.parse_args()
 
-    # check output directory
     if not os.path.exists(args.result_output_dir):
         os.makedirs(args.result_output_dir)
         logger.info(f"Directory {args.result_output_dir} created.")
 
+    if not os.path.exists(args.offload_dir):
+        os.makedirs(args.offload_dir)
+        logger.info(f"Directory {args.offload_dir} created.")
+
+    """
+    python eval_article_quality.py \
+            --input-path "../TopicPagesWiki/topics_ores_scores.csv" \
+            --gt-dir "../TopicPagesWiki" \
+            --pred-dir "/home/toapantabarahonad/storm-plus/data/baseline/refined_articles/models--snippet_ranking_model" \
+            --result-output-dir "/home/toapantabarahonad/storm-plus/results/storm_article_eval_results"
+    """
     main(args)
--- eval/evaluation_prometheus.py
+++ eval/evaluation_prometheus.py
@@ -9,14 +9,16 @@ import re
 from fastchat.conversation import get_conv_template
 from transformers import AutoTokenizer, LlamaForCausalLM
 
-logging.basicConfig(level=logging.INFO, format='%(asctime)s - %(levelname)s - %(message)s')
+logging.basicConfig(
+    level=logging.INFO, format="%(asctime)s - %(levelname)s - %(message)s"
+)
 
 
 def read_txt_file(file_path):
     """
     Read a text file to string
     """
-    with open(file_path, 'r') as file:
+    with open(file_path, "r") as file:
         return file.read()
 
 
@@ -24,7 +26,7 @@ def read_json(file_path):
     """
     Read a json file to dict
     """
-    with open(file_path, 'r') as file:
+    with open(file_path, "r") as file:
         return json.load(file)
 
 
@@ -45,13 +47,13 @@ def preprocess_text(text):
         cleaned_pargraphs.append(i)
     text = "\n".join(cleaned_pargraphs)
     # remove URLS
-    text = re.sub(r'http\S+|www\S+|https\S+', '', text, flags=re.MULTILINE)
+    text = re.sub(r"http\S+|www\S+|https\S+", "", text, flags=re.MULTILINE)
     # remove non-ascii char
-    text = re.sub(r'[^\x00-\x7F]+', '', text)
+    text = re.sub(r"[^\x00-\x7F]+", "", text)
     # remove citation bracket (e.g. [10])
-    text = re.sub(r'\[\d+\]', '', text)
+    text = re.sub(r"\[\d+\]", "", text)
     # remove non alphanumeric char
-    text = re.sub(r'[^\w\s]', '', text)
+    text = re.sub(r"[^\w\s]", "", text)
     return text
 
 
@@ -73,26 +75,30 @@ def format_prompt(prompt_template, topic, rubric, response):
     """
     prompt_template_copy = copy.deepcopy(prompt_template)
     data = copy.deepcopy(rubric)
-    data.update({
-        "instruction": f"You are a Wikipedia editor. Your task is write a wikipedia page for the topic: {topic}",
-        "response": response
-    })
+    data.update(
+        {
+            "instruction": f"You are a Wikipedia editor. Your task is write a wikipedia page for the topic: {topic}",
+            "response": response,
+        }
+    )
     filled_prompt = prompt_template_copy.format(**data)
     return get_conversation_prompt(filled_prompt)
 
 
-def get_grading_dict(responses,
-                     topic,
-                     tokenizer,
-                     model,
-                     prompt_template_path="./eval_prometheus_no_ref.prompt",
-                     rubric_path="./eval_rubric_5.json",
-                     disable_sample=False,
-                     temperature=0.01,
-                     top_p=0.95,
-                     max_new_tokens=512,
-                     repetition_penalty=1.03,
-                     logger=None):
+def get_grading_dict(
+    responses,
+    topic,
+    tokenizer,
+    model,
+    prompt_template_path="./eval_prometheus_no_ref.prompt",
+    rubric_path="./eval_rubric_5.json",
+    disable_sample=False,
+    temperature=0.01,
+    top_p=0.95,
+    max_new_tokens=512,
+    repetition_penalty=1.03,
+    logger=None,
+):
     grading = {}
     prompt_template = read_txt_file(prompt_template_path)
     rubrics = read_json(rubric_path)
@@ -104,22 +110,57 @@ def get_grading_dict(responses,
             # generate evaluation prompt and tokenize
             if logger is not None:
                 logger.info(
-                    f"processing for rubric {rubric_idx + 1}/{len(rubrics)}, response {response_idx + 1}/{len(responses)}, response length: {len(response)}")
-            prompt = format_prompt(prompt_template=prompt_template, topic=topic, rubric=rubric, response=response)
-            input_ids = tokenizer(prompt, return_tensors="pt").input_ids.to('cuda')
+                    f"processing for rubric {rubric_idx + 1}/{len(rubrics)}, response {response_idx + 1}/{len(responses)}, response length: {len(response)}"
+                )
+            prompt = format_prompt(
+                prompt_template=prompt_template,
+                topic=topic,
+                rubric=rubric,
+                response=response,
+            )
+            # input_ids = tokenizer(prompt, return_tensors="pt").input_ids
+            # .to('cuda')
             # geenrate output
-            outputs = model.generate(input_ids,
-                                     pad_token_id=tokenizer.eos_token_id,
-                                     do_sample=not disable_sample,
-                                     temperature=temperature,
-                                     top_p=top_p,
-                                     max_new_tokens=max_new_tokens,
-                                     repetition_penalty=repetition_penalty)
+            import torch
+            torch.cuda.empty_cache()
+            inputs = tokenizer(prompt, return_tensors="pt")
+
+            attention_mask = inputs["attention_mask"]
+            print(inputs["input_ids"].shape)
+            
+
+            outputs = model.generate(
+                inputs["input_ids"].to(model.device),
+                attention_mask=attention_mask,
+                pad_token_id=tokenizer.eos_token_id,
+                do_sample=not disable_sample,
+                temperature=temperature,
+                top_p=top_p,
+                max_new_tokens=max_new_tokens,
+                repetition_penalty=repetition_penalty,
+            )
+
+            # outputs = model.generate(input_ids.to(model.device),
+            #                          pad_token_id=tokenizer.eos_token_id,
+            #                          do_sample=not disable_sample,
+            #                          temperature=temperature,
+            #                          top_p=top_p,
+            #                          max_new_tokens=max_new_tokens,
+            #                          repetition_penalty=repetition_penalty)
+            # print("One generation done")
+            # outputs = outputs.cpu().numpy()
             decoded_output = tokenizer.decode(outputs[0])
+
             # decode output and format into desired fields
-            decoded_output = decoded_output[decoded_output.find("[/INST]") + len("[/INST]"):].strip()
-            feedback = decoded_output[:decoded_output.find("[RESULT]")]
-            score = decoded_output[decoded_output.find("[RESULT]") + len("[RESULT]"):].replace("</s>", "").strip()
+            decoded_output = decoded_output[
+                decoded_output.find("[/INST]") + len("[/INST]") :
+            ].strip()
+            feedback = decoded_output[: decoded_output.find("[RESULT]")]
+            score = (
+                decoded_output[decoded_output.find("[RESULT]") + len("[RESULT]") :]
+                .replace("</s>", "")
+                .strip()
+            )
             try:
                 int(score)
             except Exception as e:
@@ -128,7 +169,10 @@ def get_grading_dict(responses,
                 if match:
                     score = match.group(1)
 
-            grading[rubric["criteria_description"]][response_idx] = {"feedback": feedback, "score": score}
+            grading[rubric["criteria_description"]][response_idx] = {
+                "feedback": feedback,
+                "score": score,
+            }
     return grading
 
 
@@ -139,61 +183,107 @@ def main(args):
     doc_paths = glob.glob(os.path.join(args.batch_process_dir, "*.txt"))
     responses = [preprocess_text(read_txt_file(doc_path)) for doc_path in doc_paths]
 
-    grading = get_grading_dict(responses=responses,
-                               topic=args.topic,
-                               tokenizer=tokenizer,
-                               model=model,
-                               prompt_template_path=args.prompt_template_path,
-                               rubric_path=args.rubric_path,
-                               disable_sample=args.disable_sample,
-                               temperature=args.temperature,
-                               top_p=args.top_p,
-                               max_new_tokens=args.max_new_tokens,
-                               repetition_penalty=args.repetition_penalty,
-                               logger=logger)
+    grading = get_grading_dict(
+        responses=responses,
+        topic=args.topic,
+        tokenizer=tokenizer,
+        model=model,
+        prompt_template_path=args.prompt_template_path,
+        rubric_path=args.rubric_path,
+        disable_sample=args.disable_sample,
+        temperature=args.temperature,
+        top_p=args.top_p,
+        max_new_tokens=args.max_new_tokens,
+        repetition_penalty=args.repetition_penalty,
+        logger=logger,
+    )
 
     # Save grading dictionary to output path
-    with open(args.output_path, 'w') as outfile:
+    with open(args.output_path, "w") as outfile:
         json.dump(grading, outfile, indent=2)
         logger.info("Grading complete. Output saved to: %s", args.output_path)
 
 
 if __name__ == "__main__":
     global logger
-    parser = argparse.ArgumentParser(description='Process some files.')
-    parser.add_argument('-b', '--batch_process_dir', required=True, help='Directory of files to process')
-    parser.add_argument('-o', '--output_path', required=True, help='Path to save the output JSON file')
-    parser.add_argument('-t', "--topic", required=True, help="Topic of the script your going to analyze")
+    parser = argparse.ArgumentParser(description="Process some files.")
+    parser.add_argument(
+        "-b", "--batch_process_dir", required=True, help="Directory of files to process"
+    )
+    parser.add_argument(
+        "-o", "--output_path", required=True, help="Path to save the output JSON file"
+    )
+    parser.add_argument(
+        "-t", "--topic", required=True, help="Topic of the script your going to analyze"
+    )
 
-    parser.add_argument("--prompt_template_path", default="./eval_prometheus_no_ref.prompt",
-                        help='path to evaluation prometheus prompt template')
-    parser.add_argument("--rubric_path", default="./prompts/eval_rubric_5.json", help='path to rubric json file')
+    parser.add_argument(
+        "--prompt_template_path",
+        default="./eval_prometheus_no_ref.prompt",
+        help="path to evaluation prometheus prompt template",
+    )
+    parser.add_argument(
+        "--rubric_path",
+        default="./prompts/eval_rubric_5.json",
+        help="path to rubric json file",
+    )
 
-    parser.add_argument('--tokenizer', default="meta-llama/Llama-2-7b-chat-hf")
-    parser.add_argument('--model',
-                        choices=["kaist-ai/prometheus-13b-v1.0", "kaist-ai/prometheus-7b-v1.0"],
-                        default="kaist-ai/prometheus-13b-v1.0",
-                        help="Model to use; options are 'kaist-ai/prometheus-13b-v1.0' or 'kaist-ai/prometheus-7b-v1.0'")
-    parser.add_argument('--disable_sample', action='store_true', help='Whether to disable sampling; default is False')
-    parser.add_argument('--temperature', type=float, default=0.01, help='Temperature for generation; default is 0.01')
-    parser.add_argument('--top_p', type=float, default=0.95, help='Top P for generation; default is 0.95')
-    parser.add_argument('--max_new_tokens', type=int, default=512,
-                        help='Maximum new tokens to generate; default is 512')
-    parser.add_argument('--repetition_penalty', type=float, default=1.03, help='Repetition penalty; default is 1.03')
+    parser.add_argument("--tokenizer", default="meta-llama/Llama-2-7b-chat-hf")
+    parser.add_argument(
+        "--model",
+        choices=["kaist-ai/prometheus-13b-v1.0", "kaist-ai/prometheus-7b-v1.0"],
+        default="kaist-ai/prometheus-13b-v1.0",
+        help="Model to use; options are 'kaist-ai/prometheus-13b-v1.0' or 'kaist-ai/prometheus-7b-v1.0'",
+    )
+    parser.add_argument(
+        "--disable_sample",
+        action="store_true",
+        help="Whether to disable sampling; default is False",
+    )
+    parser.add_argument(
+        "--temperature",
+        type=float,
+        default=0.01,
+        help="Temperature for generation; default is 0.01",
+    )
+    parser.add_argument(
+        "--top_p",
+        type=float,
+        default=0.95,
+        help="Top P for generation; default is 0.95",
+    )
+    parser.add_argument(
+        "--max_new_tokens",
+        type=int,
+        default=512,
+        help="Maximum new tokens to generate; default is 512",
+    )
+    parser.add_argument(
+        "--repetition_penalty",
+        type=float,
+        default=1.03,
+        help="Repetition penalty; default is 1.03",
+    )
     args = parser.parse_args()
 
     logger = logging.getLogger(__name__)
 
-    assert os.path.exists(args.batch_process_dir), f"batch_process_dir: {args.batch_process_dir} not exists"
-    assert not os.path.isdir(args.output_path), "The specified output path is a directory. Please provide a file path."
+    assert os.path.exists(
+        args.batch_process_dir
+    ), f"batch_process_dir: {args.batch_process_dir} not exists"
+    assert not os.path.isdir(
+        args.output_path
+    ), "The specified output path is a directory. Please provide a file path."
     output_directory = os.path.dirname(args.output_path)
     if not os.path.exists(output_directory):
         os.makedirs(output_directory, exist_ok=True)
         logger.info("Created directory: %s", output_directory)
     # Check if the file exists and ask for user confirmation to override
     if os.path.exists(args.output_path):
-        overwrite = input(f"The file {args.output_path} already exists. Do you want to overwrite it? (y/n): ")
-        if overwrite.lower() != 'y':
+        overwrite = input(
+            f"The file {args.output_path} already exists. Do you want to overwrite it? (y/n): "
+        )
+        if overwrite.lower() != "y":
             logger.info("User chose not to overwrite the existing file. Exiting.")
             exit()
 
